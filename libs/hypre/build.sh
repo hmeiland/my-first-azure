c38c723a901a4ff855b25c14fe264692b9a68875
hugo meiland <hugo.meiland@microsoft.com>
2017-10-13 13:23:47 +0200

    hypre

---
+++ libs/hypre/build.sh
@@ -0,0 +1,2 @@
+wget https://computation.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/download/hypre-2.11.2.tar.gz
+
---
+++ libs/hypre/readme.txt
@@ -0,0 +1,4 @@
+
+https://computation.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/software
+
+
