72c5d2395feb1eeffa0dee190893f51ae460d87e
hugo meiland <hugo.meiland@microsoft.com>
2017-10-13 10:19:38 +0200

    build

--- libs/trilinos/build.sh
+++ libs/trilinos/build.sh
@@ -1,2 +1,30 @@
 
-wget http://trilinos.csbsju.edu/download/files/trilinos-12.12.1-Source.tar.gz
+#wget http://trilinos.csbsju.edu/download/files/trilinos-12.12.1-Source.tar.gz
+
+
+$ cd /scratch
+$ wget http://trilinos.org/oldsite/download/files/trilinos-11.12.1-Source.tar.bz2
+$ tar xvfj trilinos-11.12.1-Source.tar.bz2
+
+$ cmake \
+-D CMAKE_CXX_FLAGS:STRING=" -DMPICH_IGNORE_CXX_SEEK -DMPICH_SKIP_MPICXX" \
+-D CMAKE_BUILD_TYPE:STRING=RELEASE \
+-D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+-D Trilinos_VERBOSE_CONFIGURE:BOOL=ON \
+-D TPL_ENABLE_MPI=ON \
+-D MPI_BASE_DIR=/opt/intel/impi/4.1.3.045/intel64 \
+-D MPI_C_COMPILER:FILEPATH=mpiicc \
+-D MPI_CXX_COMPILER:FILEPATH=mpiicpc \
+-D MPI_Fortran_COMPILER:FILEPATH=mpiifort \
+-D Trilinos_ENABLE_ALL_PACKAGES=ON \
+-D BLAS_INCLUDE_DIRS:PATH=$MKLROOT/include/ \
+-D BLAS_LIBRARY_DIRS:PATH=$MKLROOT/lib/intel64/ \
+-D LAPACK_LIBRARY_DIRS:PATH=$MKLROOT/lib/intel64/ \
+-D BLAS_LIBRARY_NAMES:STRING="mkl_intel_lp64;mkl_core;mkl_sequential" \
+-D LAPACK_LIBRARY_NAMES:STRING="mkl_intel_lp64;mkl_core;mkl_sequential" \
+-D BUILD_SHARED_LIBS:BOOL=ON \
+-D CMAKE_INSTALL_PREFIX=/pfs/sw/parallel/impi_intel/trilinos-11.12.1 \
+/scratch/trilinos-11.12.1-Source
+
+$ make -j8 install
+
--- libs/trilinos/readme.txt
+++ libs/trilinos/readme.txt
@@ -1,3 +1,12 @@
 
 used:
 https://github.com/trilinos/Trilinos_tutorial/wiki/TrilinosHandsOnTutorial
+
+reference:
+https://docs.it4i.cz/software/numerical-libraries/trilinos/
+https://software.intel.com/sites/default/files/heroux-trilinos-intel-webinar.pdf
+https://pleiades.ucsc.edu/hyades/Trilinos
+
+using the ucsc doc as a baseline to build the libs...
+
+
