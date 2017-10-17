e10d0ac5c89de3d273fff62d930bc8b1cf506e26
hugo meiland <hugo.meiland@microsoft.com>
2017-10-17 09:58:18 +0200

    build script

--- libs/PETSc/build.sh
+++ libs/PETSc/build.sh
@@ -8,5 +8,6 @@ export PETSC_DIRS=`pwd`
 export PETSC_ARCH=linux-gnu-intel
 export CPATH=$CPATH:/opt/intel/compilers_and_libraries_2018.0.128/linux/mpi/intel64/include 
 ./configure --with-blas-lapack-dir=$MKLROOT
-make
+make PETSC_DIR=/home/hugo/my-first-azure/libs/PETSc/petsc-3.8.0 PETSC_ARCH=linux-gnu-intel all
+make PETSC_DIR=/home/hugo/my-first-azure/libs/PETSc/petsc-3.8.0 PETSC_ARCH=linux-gnu-intel test
 
