1e9e104779b1a11e7bb0e3e70a622570e75d0145
hugo meiland <hugo.meiland@microsoft.com>
2017-10-17 09:44:48 +0200

    build script

--- libs/PETSc/build.sh
+++ libs/PETSc/build.sh
@@ -1,8 +1,12 @@
 
 wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.8.0.tar.gz
-#tar zxvf hypre-2.11.2.tar.gz
+tar zxf petsc-lite-3.8.0.tar.gz
 
-#cd hypre-2.11.2/src
-#./configure
-#make
+cd petsc-3.8.0 
+
+export PETSC_DIRS=`pwd`
+export PETSC_ARCH=linux-gnu-intel
+export CPATH=$CPATH:/opt/intel/compilers_and_libraries_2018.0.128/linux/mpi/intel64/include 
+./configure --with-blas-lapack-dir=$MKLROOT
+make
 
