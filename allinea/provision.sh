240823d61e03bf8eb8ed50eb96d6d472717125f1
hugo meiland <hugo.meiland@microsoft.com>
2017-10-26 11:30:00 +0200

    ddt install

---
+++ allinea/provision.sh
@@ -0,0 +1,8 @@
+
+sudo yum -y libXext libXrender fontconfig libSM xauth xclock
+
+wget http://content.allinea.com/downloads/allinea-forge-latest-Redhat-7.0-x86_64.tar
+
+tar xvf allinea-forge-latest-Redhat-7.0-x86_64.tar
+cd allinea-forge-7.1-Redhat-7.0-x86_64/
+./textinstaller.sh
