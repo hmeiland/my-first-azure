1547fd942ef129cf6652c70477696205f3a2575d
hugo meiland <hugo.meiland@microsoft.com>
2017-10-27 16:36:18 +0200

    bla

--- IOR/raid.sh
+++ IOR/raid.sh
@@ -1,3 +1,5 @@
 DISKS=`ls /dev/sd* | grep -v sda | grep -v sdb`
+NUMDISKS=`echo $DISKS | wc`
+echo $DISKS $NUMDISKS
 
-echo $DISKS
+echo sudo mdadm --create --verbose /dev/md0 --level=0 --raid-devices=2 /dev/sda /dev/sdb
