604e3142b0356a179e6cdd5b1301eb35cef43aaa
hugo meiland <hugo.meiland@microsoft.com>
2017-10-27 16:40:44 +0200

    test

--- IOR/raid.sh
+++ IOR/raid.sh
@@ -1,5 +1,8 @@
 DISKS=`ls /dev/sd* | grep -v sda | grep -v sdb`
-NUMDISKS=`echo $DISKS | wc`
+NUMDISKS=`echo $DISKS | wc -w`
 echo $DISKS $NUMDISKS
 
-echo sudo mdadm --create --verbose /dev/md0 --level=0 --raid-devices=2 /dev/sda /dev/sdb
+sudo mdadm --create --verbose /dev/md0 --level=0 --raid-devices=$NUMDISKS $DISKS
+sudo mkfs.ext4 /dev/md0
+sudo mount /dev/md0 /mnt/resource
+sudo chmod 777 /mnt/resource
