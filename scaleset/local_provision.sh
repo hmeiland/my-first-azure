0c9ac48f7b9873771119ff37fca07e7856b92233
hugo meiland <hugo.meiland@microsoft.com>
2017-10-31 12:12:42 +0100

    test

--- scaleset/local_provision.sh
+++ scaleset/local_provision.sh
@@ -1,26 +1,26 @@
 
 
-yum -y install git nfs-utils
+sudo yum -y install git nfs-utils
 
 
-git clone https://github.com/hmeiland/my-first-azure
+sudo -u hugo git clone https://github.com/hmeiland/my-first-azure
 cd my-first-azure/HPL
 
-echo "source ~/my-first-azure/HPL/env.sh" >> /etc/bashrc
-echo "export I_MPI_FABRICS=dapl I_MPI_DAPL_PROVIDER=ofa-v2-ib0 I_MPI_DYNAMIC_CONNECTION=0" >> /etc/bashrc
+sudo echo "source ~/my-first-azure/HPL/env.sh" >> /etc/bashrc
+sudo echo "export I_MPI_FABRICS=dapl I_MPI_DAPL_PROVIDER=ofa-v2-ib0 I_MPI_DYNAMIC_CONNECTION=0" >> /etc/bashrc
 
 if [ `hostname -s` == "node1" ]; then
 
 echo "I am node1!!!"
-echo "/opt 10.0.0.0/16(rw)" > /etc/exports
+sudo echo "/opt 10.0.0.0/16(rw)" > /etc/exports
+sudo echo "/home 10.0.0.0/16(rw)" >> /etc/exports
 sudo service nfs restart 
 ./build.sh
 fi
 
 
 if [ `hostname -s` != "node1" ]; then
-
-mount 10.0.0.4:/opt /opt
+sudo mount 10.0.0.4:/opt /opt
 ./build.sh
 fi
 
