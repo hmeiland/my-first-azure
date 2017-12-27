3a8dcdde3f676e710e75dcd95f85cda33dcf6ce7
hugo meiland <hugo.meiland@microsoft.com>
2017-12-27 12:47:40 +0100

    enable rdma in ubuntu

--- scripts/ubuntu_enable_rdma.sh
+++ scripts/ubuntu_enable_rdma.sh
@@ -1,4 +1,4 @@
-sudo apt-get install libdapl2 libmlx4-1
+sudo apt-get -y install libdapl2 libmlx4-1
 sudo sed -i -e 's/# OS.EnableRDMA=y/OS.EnableRDMA=y/' /etc/waagent.conf
 sudo sed -i -e 's/# OS.UpdateRdmaDriver=y/OS.UpdateRdmaDriver=y/' /etc/waagent.conf
 sudo service walinuxagent restart
