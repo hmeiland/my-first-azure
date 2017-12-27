1f67f0964539d53e9d501a486f8fc2631cca0785
hugo meiland <hugo.meiland@microsoft.com>
2017-12-27 13:09:05 +0100

    enable rdma in ubuntu

--- scripts/ubuntu_enable_rdma.sh
+++ scripts/ubuntu_enable_rdma.sh
@@ -3,6 +3,6 @@ sudo sed -i -e 's/# OS.EnableRDMA=y/OS.EnableRDMA=y/' /etc/waagent.conf
 sudo sed -i -e 's/# OS.UpdateRdmaDriver=y/OS.UpdateRdmaDriver=y/' /etc/waagent.conf
 sudo sed -i -e 's/kernel.yama.ptrace_scope = 1/kernel.yama.ptrace_scope = 0/' /etc/sysctl.d/10-ptrace.conf
 sudo sysctl -w kernel.yama.ptrace_scope=0
-sudo echo "*		hard	memlock		unlimited" >> /etc/security/limits.conf
-sudo echo "*		soft	memlock		unlimited" >> /etc/security/limits.conf
+sudo bash -c 'echo "*		hard	memlock		unlimited" >> /etc/security/limits.conf'
+sudo bash -c 'echo "*		soft	memlock		unlimited" >> /etc/security/limits.conf'
 sudo service walinuxagent restart
